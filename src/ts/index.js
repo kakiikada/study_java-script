f8ce92c83c16aff9d5f66dd4efe9b12f39ddbd41
kakiikada <kakiikada@gmail.com>
2026-06-29 03:55:42 +0900

    トップページ作成

--- src/ts/index.js
+++ src/ts/index.js
@@ -1,5 +1,55 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const group = "牡蠣の養殖場";
-console.log(`ここは${group}です`);
+// headerアイコン取得
+const openMenu = document.getElementById("js-header_navIcon");
+openMenu.addEventListener('click', (event) => {
+    if (openMenu && openMenu.classList.contains('js-active')) {
+        openMenu.classList.remove("js-active");
+    }
+    else {
+        openMenu.classList.add("js-active");
+    }
+});
+const menuLink = document.getElementById("js-header_navLink");
+// 子要素を取得
+const menuLinkBtn = menuLink.querySelectorAll('.js-header-navLink');
+for (const menuLinkBtnitem of menuLinkBtn) {
+    menuLinkBtnitem.addEventListener('click', (event) => {
+        if (openMenu && openMenu.classList.contains('js-active')) {
+            openMenu.classList.remove("js-active");
+        }
+    });
+}
+// モーダルを開く
+const modal = document.getElementById("js-modal");
+// 子要素を取得
+const modalBtn = modal.querySelectorAll('.js-modalItem');
+const modalFlame = document.getElementById("js-modalFlame");
+for (const modalItem of modalBtn) {
+    modalItem.addEventListener('click', (event) => {
+        if (modalFlame && modalFlame.classList.contains('js-active')) {
+            // modalFlame.classList.remove("js-active");
+        }
+        else {
+            modalFlame.classList.add("js-active");
+        }
+    });
+}
+// モーダルを閉じる
+// ×ボタン
+// const modalClose = document.getElementById("js-modalFlame_close"); 
+//     modalClose.addEventListener('click', (event: MouseEvent) => {
+//     if(modalFlame && modalFlame.classList.contains('js-active')){
+//                 modalFlame.classList.remove("js-active");
+//     } else{
+//         modalFlame.classList.add("js-active");
+//     }
+// });
+modalFlame.addEventListener('click', (event) => {
+    if (modalFlame && modalFlame.classList.contains('js-active')) {
+        modalFlame.classList.remove("js-active");
+    }
+    else {
+        // modalFlame.classList.add("js-active");
+    }
+});
+export {};
 //# sourceMappingURL=index.js.map
--- src/ts/index.ts
+++ src/ts/index.ts
@@ -1,4 +1,52 @@
-const group: string = "牡蠣の養殖場";
+// headerアイコン取得
+const openMenu = document.getElementById("js-header_navIcon"); 
+openMenu.addEventListener('click', (event: MouseEvent) => {
+    if(openMenu && openMenu.classList.contains('js-active')){
+        openMenu.classList.remove("js-active");
+    }else{
+        openMenu.classList.add("js-active");
+    }
+});
 
-console.log(`ここは${group}です`);
+const menuLink = document.getElementById("js-header_navLink"); 
+// 子要素を取得
+const menuLinkBtn = menuLink.querySelectorAll<HTMLElement>('.js-header-navLink');
+for (const menuLinkBtnitem of menuLinkBtn) {
+    menuLinkBtnitem.addEventListener('click', (event: MouseEvent) => {
+        if(openMenu && openMenu.classList.contains('js-active')){
+            openMenu.classList.remove("js-active");
+        }
+    });
+}
 
+// モーダルを開く
+const modal = document.getElementById("js-modal"); 
+// 子要素を取得
+const modalBtn = modal.querySelectorAll<HTMLElement>('.js-modalItem');
+const modalFlame = document.getElementById("js-modalFlame"); 
+for (const modalItem  of modalBtn ) {
+     modalItem.addEventListener('click', (event: MouseEvent) => {
+        if(modalFlame && modalFlame.classList.contains('js-active')){
+            // modalFlame.classList.remove("js-active");
+        } else{
+            modalFlame.classList.add("js-active");
+        }
+    });   
+}
+// モーダルを閉じる
+// ×ボタン
+// const modalClose = document.getElementById("js-modalFlame_close"); 
+//     modalClose.addEventListener('click', (event: MouseEvent) => {
+//     if(modalFlame && modalFlame.classList.contains('js-active')){
+//                 modalFlame.classList.remove("js-active");
+//     } else{
+//         modalFlame.classList.add("js-active");
+//     }
+// });
+    modalFlame.addEventListener('click', (event: MouseEvent) => {
+    if(modalFlame && modalFlame.classList.contains('js-active')){
+        modalFlame.classList.remove("js-active");
+    } else{
+        // modalFlame.classList.add("js-active");
+    }
+});   
